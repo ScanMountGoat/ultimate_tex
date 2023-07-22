44a782c03faeff84641c56b95c41ffc4ab8a735c
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-07-21 21:23:56 -0500

    set version in tauri window title

--- ultimate_tex_tauri/src-tauri/src/main.rs
+++ ultimate_tex_tauri/src-tauri/src/main.rs
@@ -1,9 +1,8 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use serde::Serialize;
+use tauri::{Manager, WindowEvent};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[derive(Serialize, Clone)]
@@ -33,6 +32,20 @@ fn load_items() -> Vec<Item> {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            app.get_window("main")
+                .unwrap()
+                .set_title(concat!("Ultimate Tex ", env!("CARGO_PKG_VERSION")))
+                .unwrap();
+            Ok(())
+        })
+        .on_window_event(|e| {
+            // Workaround for slow Chromium window resizing.
+            // https://github.com/tauri-apps/tauri/issues/6322
+            if let WindowEvent::Resized(_) = e.event() {
+                std::thread::sleep(std::time::Duration::from_nanos(1));
+            }
+        })
         .invoke_handler(tauri::generate_handler![load_items])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
